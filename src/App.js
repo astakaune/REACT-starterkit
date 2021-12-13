d23b2d196ed5c0ffe7a6254252deb68b3a876600
astakaune <asta.kaune@gmail.com>
2021-12-13 17:09:00 +0200

    firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 // import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
+import "./index.scss";
 
 function App() {
   return (
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+			// Your web app's Firebase configuration (example):
+			const firebaseConfig = {
+			apiKey: "AIzaSyD3lyihPkRoHB8JyMv3vr7uKBDIRtdxmK0",
+			authDomain: "timetableapp-bb4a2.firebaseapp.com",
+			projectId: "timetableapp-bb4a2",
+			storageBucket: "timetableapp-bb4a2.appspot.com",
+			messagingSenderId: "19170589489",
+			appId: "1:19170589489:web:adfc5cc0343a902053ce10"
+			};
+			// Initialize Firebase
+			const app = initializeApp(firebaseConfig);
